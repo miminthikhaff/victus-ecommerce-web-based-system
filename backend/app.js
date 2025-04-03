11d7ee03b338a4051db00940db85d159510e62ba
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-04-03 17:34:04 +0530

    feat: enable CORS for frontend requests and reorganize static file serving

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,22 @@ const express = require("express");
 const app = express();
 const ErrorHandler = require("./middleware/error");
 const cookieParser = require("cookie-parser");
-const fileUpload = require("express-fileupload");
 const bodyParser = require("body-parser");
 const path = require("path");
+const cors = require('cors');
+
+app.use(cors({
+    origin: "http://localhost:3000", // Allow frontend to make requests to backend
+    methods: "GET,POST,PUT,DELETE",  // Allowed HTTP methods
+    credentials: true
+  
+}));
+
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(fileUpload({useTempFiles: true}));
+
 
 // config
 if(process.env.NODE_ENV!=="PRODUCTION"){
@@ -25,6 +32,7 @@ const order = require("./routes/OrderRoute");
 const payment = require("./routes/PaymentRoute");
 const cart = require("./routes/WishListRoute");
 
+// Define API routes first
 app.use("/api/v2",product);
 
 app.use("/api/v2",user);
@@ -35,14 +43,16 @@ app.use("/api/v2",payment);
 
 app.use("/api/v2",cart);
 
+// Serve React static files only after API routes
 app.use(express.static(path.join(__dirname,"../frontend/build")));
 
+// Fallback for any unhandled routes (React app)
 app.get("*",(req,res) =>{
     res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
 })
 
-// Serve the uploaded files
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
 
 
 // it's for errorHandeling
