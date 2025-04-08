3edc60db39d4995f52146c2ca1df0521b5623895
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-04-08 14:35:01 +0530

    fix: wrap App component with BrowserRouter for routing support

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
+import { Provider } from 'react-redux';
 import store from './store';
+import { BrowserRouter } from 'react-router-dom'; // ✅ import this
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter basename="/"> {/* ✅ Add this wrapper */}
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
